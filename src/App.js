ec43f6dbb94e0691f6082835c605d2e66e8e11c1
cladams <cladams0203@gmail.com>
2019-08-13 12:34:22 -0400

    started with state in app for timer display

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+
+
+function App() {
+  const [timer, setTimer] = useState(0)
+
+  return (
+    <div className="App">
+      <header>The Fantasy Football Draft Clock</header>
+
+    </div>
+  );
+}
+
+export default App;
